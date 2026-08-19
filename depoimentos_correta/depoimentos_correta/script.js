9f91d7bfe60b9aec426a4efe717ada4b4bc26997
cerqueiraproj2552 <thiago.cerqueira.nascimento@escola.pr.gov.br>
2026-08-19 14:57:00 -0300

    Delete depoimentos_correta/depoimentos_correta directory

--- depoimentos_correta/depoimentos_correta/script.js
+++
@@ -1,4 +0,0 @@
-(function(){'use strict';
-var b=document.getElementById('menuBotao'),l=document.getElementById('menuLista');
-if(b&&l){b.addEventListener('click',function(){var o=l.classList.toggle('aberto');b.setAttribute('aria-expanded',o?'true':'false')})}
-})();
